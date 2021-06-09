d0f1ab3d22cfd3567f0e7e4c2c1bd70aa2c9f7b9
4770K <koninpavlik@gmail.com>
2021-06-09 20:48:01 +0300

    'init'

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDownCriteries = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDownElements = new System.Windows.Forms.NumericUpDown();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonRandomize = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCriteries)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownElements)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDownCriteries);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(139, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Количество критериев";
+            // 
+            // numericUpDownCriteries
+            // 
+            this.numericUpDownCriteries.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDownCriteries.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownCriteries.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownCriteries.Name = "numericUpDownCriteries";
+            this.numericUpDownCriteries.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownCriteries.TabIndex = 0;
+            this.numericUpDownCriteries.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDownCriteries.ValueChanged += new System.EventHandler(this.numericUpDownCriteries_ValueChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.numericUpDownElements);
+            this.groupBox2.Location = new System.Drawing.Point(157, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(139, 52);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Количество элементов";
+            // 
+            // numericUpDownElements
+            // 
+            this.numericUpDownElements.Location = new System.Drawing.Point(6, 19);
+            this.numericUpDownElements.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDownElements.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownElements.Name = "numericUpDownElements";
+            this.numericUpDownElements.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownElements.TabIndex = 0;
+            this.numericUpDownElements.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDownElements.ValueChanged += new System.EventHandler(this.numericUpDownElements_ValueChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dataGridView1);
+            this.groupBox4.Location = new System.Drawing.Point(12, 70);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(855, 315);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Ввод значений";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(844, 290);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.buttonRandomize);
+            this.groupBox3.Location = new System.Drawing.Point(302, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(137, 52);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Рандомизировать";
+            // 
+            // buttonRandomize
+            // 
+            this.buttonRandomize.Location = new System.Drawing.Point(6, 19);
+            this.buttonRandomize.Name = "buttonRandomize";
+            this.buttonRandomize.Size = new System.Drawing.Size(124, 23);
+            this.buttonRandomize.TabIndex = 0;
+            this.buttonRandomize.Text = "Рандомизировать";
+            this.buttonRandomize.UseVisualStyleBackColor = true;
+            this.buttonRandomize.Click += new System.EventHandler(this.buttonRandomize_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.buttonCalculate);
+            this.groupBox5.Location = new System.Drawing.Point(445, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(137, 52);
+            this.groupBox5.TabIndex = 6;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Рассчитать";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(6, 19);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(124, 23);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Рассчитать";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.dataGridView2);
+            this.groupBox6.Location = new System.Drawing.Point(12, 391);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(855, 315);
+            this.groupBox6.TabIndex = 5;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "Результат";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(844, 290);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 744);
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Алгоритм Парето. Конин П.А. Вариант 4";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCriteries)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownElements)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownCriteries;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDownElements;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button buttonRandomize;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fillGrid();
+        }
+
+        private void fillGrid()
+        {
+            for (int i = 0; i < numericUpDownCriteries.Value; i++)
+            {
+                dataGridView1.Columns.Add(i.ToString(), i.ToString());
+                dataGridView1.Columns[i].DefaultCellStyle.NullValue = "0";
+            }
+            for (int i = 0; i < numericUpDownElements.Value; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+
+        private void numericUpDownCriteries_ValueChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(((UpDownBase)sender).Text) < numericUpDownCriteries.Value)
+            {
+                dataGridView1.Rows.Add();
+            }
+            else
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 1);
+            }
+        }
+
+        private void numericUpDownElements_ValueChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(((UpDownBase)sender).Text) < numericUpDownElements.Value)
+            {
+                dataGridView1.Columns.Add((dataGridView1.Columns.Count).ToString(), (dataGridView1.Columns.Count).ToString());
+                dataGridView1.Columns[dataGridView1.Columns.Count - 1].DefaultCellStyle.NullValue = "0";
+                
+            }
+            else
+            {
+                dataGridView1.Columns.Remove((Convert.ToInt32(((UpDownBase)sender).Text) - 1).ToString());
+            }
+        }
+
+        private void buttonRandomize_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                {
+                    dataGridView1.Rows[j].Cells[i].Value = rnd.Next(0, 15).ToString();
+                }
+            }
+        }
+
+        private static double GetSumOfVector(List<double> vector)
+        {
+            double sum = 0;
+            foreach (var element in vector)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+
+        private static List<List<double>> dgwTo2dListOfDouble(DataGridView dataGridView)
+        {
+            List<List<double>> list = new List<List<double>>();
+            for (int i2 = 0; i2 < dataGridView.Rows.Count; i2++)
+            {
+                list.Add(new List<double>());
+                for (int j2 = 0; j2 < dataGridView.Columns.Count; j2++)
+                {
+                    list[i2].Add(Convert.ToDouble(dataGridView.Rows[i2].Cells[j2].Value));
+                }
+            }
+            return list;
+        }
+
+        private static List<List<double>> Pareto(List<List<double>> matrix)
+        {
+            List<List<double>> paretoSet = new List<List<double>>();
+
+            matrix.ForEach(v => paretoSet.Add(v));
+
+            int i = 0;
+            int j = 1;
+
+        step2:
+            if (j == matrix.Count) return paretoSet;
+            if (GetSumOfVector(matrix[i]) > GetSumOfVector(matrix[j]))
+            {
+                paretoSet.Remove(matrix[j]);
+                if (j < matrix.Count)
+                {
+                    j++;
+                    goto step2;
+                }
+                else
+                {
+                    goto step7;
+                }
+            }
+            else
+            {
+                if (j == matrix.Count) return paretoSet;
+                if (GetSumOfVector(matrix[j]) > GetSumOfVector(matrix[i]))
+                {
+                    paretoSet.Remove(matrix[i]);
+                    goto step7;
+                }
+                else
+                {
+                    if (j < matrix.Count)
+                    {
+                        j++;
+                        goto step2;
+                    }
+                    else
+                    {
+                        goto step7;
+                    }
+                }
+            }
+
+        step7:
+            if (i < matrix.Count - 1)
+            {
+                i++;
+                j = i + 1;
+                goto step2;
+            }
+            else
+            {
+                return paretoSet;
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView2.Refresh();
+
+            List<List<double>> matrix = dgwTo2dListOfDouble(dataGridView1);
+
+            List<List<double>> result = Pareto(matrix);
+
+            for (int i = 0; i < numericUpDownElements.Value; i++)
+            {
+                dataGridView2.Columns.Add(i.ToString(), i.ToString());
+                dataGridView2.Columns[i].DefaultCellStyle.NullValue = "0";
+            }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                dataGridView2.Rows.Add();
+                for (int j = 0; j < result[i].Count; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = result[i][j];
+                }
+            }
+        }
+    }
+}
